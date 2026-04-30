5485f14cf0d8e67ed1fb560792f818e4037ac507
Juan Gutiérrez <juan.gutierrez.1@ug.uchile.cl>
2026-04-30 13:57:54 -0400

    Limpieza 2015 terminada, lista para el analisis

---
+++ 3a_preparacion_2015.R
@@ -0,0 +1,27 @@
+
+pacman::p_load(haven, dplyr, srvyr)
+
+#cargamos base de datos 
+casen_2015 <- read_dta("input/data/original/casen_2015.dta")
+
+names(casen_2015)
+
+
+casen_limpia_2015 <- casen_2015 %>% 
+  filter(nucleo == 1) %>% 
+  select(id_vivienda, hogar, region, 
+    pobreza_2013,  # Variable oficial para la serie histórica[cite: 1, 2]
+    varunit, varstrat, 
+    expr, numper, ytotcorh)
+
+#formual sincronización metodologia 2024
+
+casen_limpia_2015 <- casen_limpia_2015 %>% 
+  mutate(
+    nae = numper^0.7,
+    yae = round(ytotcorh / nae)
+  )
+
+saveRDS(casen_limpia_2015, "input/data/proc/data_preparada_2015.rds")
+
+
